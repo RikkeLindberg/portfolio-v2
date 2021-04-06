2da4ac4c599e29d36c3285c78aed8e5e4196639b
RikkeLindberg <lindbergxrikke@gmail.com>
2021-04-06 15:14:18 +0200

    add own api

---
+++ pages/api/courses.js
@@ -0,0 +1,16 @@
+export const courses = [
+    {
+        'id': 1,
+        'name': 'Advanced CSS and Sass',
+        'link': 'https://www.udemy.com/course/advanced-css-and-sass/',
+        'certificate': '',
+        'progress': '100%',
+    },
+    {
+        'id': 2,
+        'name': 'JavaScript Simplified',
+        'link': 'https://courses.webdevsimplified.com/',
+        'certificate': null,
+        'progress': '0%',
+    },
+]
---
+++ pages/api/projects.js
@@ -0,0 +1,42 @@
+export const projects = [
+    {
+        'id': 1,
+        'name': 'SpaceX',
+        'thumbnail': '',
+        'link_live': '',
+        'github_repo': '',
+        'technologies': ['HTML', 'Sass'],
+        'description': '',
+    },
+    {
+        'id': 2,
+        'name': 'Letha',
+        'thumbnail': '',
+        'link_live': '',
+        'github_repo': '',
+        'technologies': ['HTML', 'Sass'],
+        'description': '',
+    },
+    {
+        'id': 3,
+        'name': 'Re-cycle',
+        'thumbnail': '',
+        'link_live': '',
+        'github_repo': '',
+        'technologies': ['HTML', 'Sass'],
+        'description': '',
+    },
+    {
+        'id': 3,
+        'name': 'Community science museum',
+        'thumbnail': '',
+        'link_live': '',
+        'github_repo': '',
+        'technologies': ['HTML', 'Sass'],
+        'description': '',
+    },
+]
+
+
+
+
