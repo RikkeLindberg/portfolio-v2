4443214f4ad7a169e0dc9b1f97bedc62741c1b0b
RikkeLindberg <lindbergxrikke@gmail.com>
2021-04-06 15:16:44 +0200

    change file path

---
+++ components/layout/Container.js
@@ -0,0 +1,21 @@
+import PropTypes from "prop-types";
+import Navbar from "./navbar/Navbar";
+import Footer from "./Footer";
+
+export default function Container({ children }) {
+    return (
+        <>
+            <Navbar/>
+                <main className="wrapper">
+                    <div className="container">
+                        { children }
+                    </div>
+                </main>
+            <Footer />
+        </>
+    )
+}
+
+Container.propTypes = {
+    children: PropTypes.element.isRequired,
+};
