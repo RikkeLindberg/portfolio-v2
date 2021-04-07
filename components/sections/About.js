0cf09d636b7ae5934bfae1a440e6ab06d35023bf
RikkeLindberg <lindbergxrikke@gmail.com>
2021-04-07 18:54:01 +0200

    add AboutMe component

--- components/sections/About.js
+++ components/sections/About.js
@@ -1,9 +1,11 @@
+import AboutMe from "../about/AboutMe";
 import Heading from "../typography/Heading";
 
 export default function About() {
     return (
         <section id="about">
             <Heading size="2" title="about"/>
+            <AboutMe />
         </section>
     )
 }
