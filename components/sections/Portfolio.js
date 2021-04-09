3c84099980f2d90b011f0c8ce6c6c8f5905569b3
RikkeLindberg <lindbergxrikke@gmail.com>
2021-04-09 09:31:39 +0200

    add Projects

--- components/sections/Portfolio.js
+++ components/sections/Portfolio.js
@@ -1,9 +1,11 @@
+import Projects from "../project/Projects";
 import Heading from "../typography/Heading";
 
 export default function Portfolio() {
     return (
         <section id="portfolio">
             <Heading size="2" title="portfolio"/>
+            <Projects />
         </section>
     )
 }
