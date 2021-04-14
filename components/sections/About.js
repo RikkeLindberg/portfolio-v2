74787a67a2567b33789f86a6612d3a8a5a47c554
RikkeLindberg <lindbergxrikke@gmail.com>
2021-04-14 12:38:12 +0200

    change header title

--- components/sections/About.js
+++ components/sections/About.js
@@ -6,7 +6,7 @@ import styles from './About.module.css';
 export default function About() {
     return (
         <section id="about" className={styles.section}>
-            <Heading size="2" title="about me"/>
+            <Heading size="2" title="about"/>
             <AboutMe />
             <Experiences />
         </section>
