d6c25e2dada24e424693b73f7d53b8037be69ba3
RikkeLindberg <lindbergxrikke@gmail.com>
2021-04-14 12:37:23 +0200

    change class name and order

--- components/about/experiences/Work.js
+++ components/about/experiences/Work.js
@@ -4,19 +4,19 @@ import styles from './Experiences.module.css';
 
 export default function Work() {
     return (
-        <div className={styles.work}>
+        <div className={styles.container}>
             <Heading size="3" title="Work experience" />
 
-            <div>
-                <Heading size="4" title="2011-2013" />
-                <Heading size="4" title="Rema 1000 Myre" />
-                <Paragraph content="Deltids ansatt, helger og ferier" />
-            </div>
-
-            <div>
+            <div className={styles.column}>
                 <Heading size="4" title="2013-2019" />
                 <Heading size="4" title="Rema 1000 Sortland" />
                 <Paragraph content="Heltids ansatt, skift- og bestillings ansvarlig" />
+            </div>
+
+            <div className={styles.column}>
+                <Heading size="4" title="2011-2013" />
+                <Heading size="4" title="Rema 1000 Myre" />
+                <Paragraph content="Deltids ansatt, helger og ferier" />
             </div>
         </div>
     )
