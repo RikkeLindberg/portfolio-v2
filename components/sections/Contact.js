6f241e8e624ffa5ddfe9cd77f492a3cb05708bcf
RikkeLindberg <lindbergxrikke@gmail.com>
2021-04-14 11:00:11 +0200

    add contact form component

--- components/sections/Contact.js
+++ components/sections/Contact.js
@@ -1,10 +1,10 @@
-import ContactForm from "../ContactForm";
-import Heading from "../typography/Heading";
+import ContactForm from '../form/ContactForm';
+import Heading from '../typography/Heading';
 
 export default function Contact() {
     return (
         <section id="contact">
-            <Heading size="2" title="contact"/>
+            <Heading size="2" title="contact me"/>
             <ContactForm />
         </section>
     )
