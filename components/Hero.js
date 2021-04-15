9e1bd8ae6e3ed073a5713f99a25a16b897b9b8ca
RikkeLindberg <lindbergxrikke@gmail.com>
2021-04-15 20:59:38 +0200

    change title for heading 2

--- components/Hero.js
+++ components/Hero.js
@@ -5,8 +5,8 @@ import styles from './Hero.module.css';
 export default function Hero() {
     return (
         <div className={styles.hero}>
-            <Heading size="1" title="Rikke Lindberg"/>
-            <Heading size="2" title="Front-End Developer"/>
+            <Heading size="1" title="Rikke lindberg"/>
+            <Heading size="2" title="Junior front-end developer"/>
             <Button link="#portfolio">View my portfolio</Button>
         </div>
     )
