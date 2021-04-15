eb5950b0061156a128041d0d92f63a59690903bb
RikkeLindberg <lindbergxrikke@gmail.com>
2021-04-15 20:46:37 +0200

    add css module

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -1,8 +1,10 @@
+import styles from './Footer.module.css';
+
 export default function Footer() {
     const currentYear = new Date().getFullYear();
 
     return (
-        <footer className="footer">
+        <footer className={styles.footer}>
           <small>{currentYear} &copy; Rikke Lindberg. All rights reserved.</small>
         </footer>
     )
