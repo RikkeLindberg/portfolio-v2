e5ae401ffb762e6ecc74448b9e3f009cb6eae3c7
RikkeLindberg <lindbergxrikke@gmail.com>
2021-04-15 20:50:42 +0200

    code formatting

--- components/form/ContactForm.js
+++ components/form/ContactForm.js
@@ -71,39 +71,44 @@ export default function ContactForm() {
             )}
             {!status.info.error && status.info.msg && <p>{status.info.msg}</p>}
         </div>
+
         <form className={styles.form} onSubmit={handleOnSubmit}>
-        <label htmlFor="name">Name</label>
-            <input
+          <label htmlFor="name">Name</label>
+          <input
             id="name"
             type="text"
             onChange={handleOnChange}
             required
             value={inputs.name}
-            />
-            <label htmlFor="email">Email</label>
-            <input
+          />
+
+          <label htmlFor="email">Email</label>
+          <input
             id="email"
             type="email"
             name="_replyto"
             onChange={handleOnChange}
             required
             value={inputs.email}
-            />
-            <label htmlFor="message">Message</label>
-            <textarea
+          />
+
+          <label htmlFor="message">Message</label>
+          <textarea
             id="message"
             name="message"
             onChange={handleOnChange}
             required
             value={inputs.message}
-            />
-            <Button type="submit" disabled={status.submitting}>
-            {!status.submitting
-                ? !status.submitted
-                ? 'Submit'
-                : 'Submitted'
-                : 'Submitting...'}
-            </Button>
+          />
+
+          <Button type="submit" disabled={status.submitting}>
+            { !status.submitting
+              ? !status.submitted
+              ? 'Submit'
+              : 'Submitted'
+              : 'Submitting...'
+            }
+          </Button>
         </form>
     </>
   )
